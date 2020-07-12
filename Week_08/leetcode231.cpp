ba0d559ad56dcb3df882524140381500aefe4418
liguochen <qq975183533@gmail.com>
2020-07-12 14:07:16 +0800

    leetcode习题2的幂提交

---
+++ Week_08/leetcode231.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        long x = n;
+        return (x & (x - 1)) == 0;
+    }
+};
